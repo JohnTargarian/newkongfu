a820d0c681788d120f3b944da98b3cfc07ae4abb
tujiuxu <tjx_bigdata@126.com>
2020-08-18 23:08:47 +0800

    11223

---
+++ src/main/java/com/atguigu/jiuyangzaitai.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+public class jiuyangzaitai {
+    public static void main(String[] args) {
+        System.out.println("九阳神功第九重：九阳在天!");
+        System.out.println("九阳神功第九重：九阳在天!");
+        System.out.println("九阳神功第九重：九阳在天！");
+    }
+}
